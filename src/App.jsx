122bb4bd1e21a0c726e7fca241fe083b8a6f132d
moeketsi <raymondkxng17@gmail.com>
2022-08-29 17:05:47 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import styles from './style';
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
+      {/* navbar section */}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           Navbar
@@ -11,6 +12,25 @@ const App = () => {
       </div>
 
       {/* hero section */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+      {/* hero section */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Business
+          Billing
+          card
+          testimonial
+          client
+          ctA
+          footer
+        </div>
+      </div>
     </div>
   )
 }
